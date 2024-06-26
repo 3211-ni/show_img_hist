fb625006c1cc48f76ef91126ca900354a6adf234
Taisuke Iwata <32114024@stn.nitech.ac.jp>
2024-06-26 16:20:48 +0900

    for kadai9_2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons, now at 2024/6/26'
+        caption='image from wikimedia commons, now at 2024/06/26'
         use_column_width=True
     )
 
